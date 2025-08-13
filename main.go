0822389fb3f56ea510689667f37a8241a6a55ac0
lalobgc <lalobg.c@gmail.com>
2025-08-13 13:08:56 -0600

    Reordering as bootdev files

--- main.go
+++ main.go
@@ -1,23 +1,11 @@
 package main
 
 import (
-  "fmt"
-  "bufio"
-  "os"
   //"strings"
 )
 
 func main()  {
-  scanner := bufio.NewScanner(os.Stdin)
-  var input string
-  for true {
-    fmt.Print("Pokedex > ")
-    if scanner.Scan() {
-      input = scanner.Text()
-    }
-    firstWord := cleanInput(input)[0]
-    fmt.Printf("Your command was: %s \n", firstWord)
-  }
+  startRepl()
 }
 
 
--- repl.go
+++ repl.go
@@ -2,7 +2,26 @@ package main
 
 import (
   "strings"
+  "fmt"
+  "bufio"
+  "os"
 )
+
+func startRepl() {
+  scanner := bufio.NewScanner(os.Stdin)
+  var input string
+  for true {
+    fmt.Print("Pokedex > ")
+    if scanner.Scan() {
+      input = scanner.Text()
+      if len(input) == 0 {
+        continue
+      }
+      firstWord := cleanInput(input)[0]
+      fmt.Printf("Your command was: %s \n", firstWord)
+    }
+  }
+}
 
 func cleanInput(text string) []string {
   var splitText []string
